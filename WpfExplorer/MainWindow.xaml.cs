3a169506b4d74d8e9131703c5e09b422b1220a28
VenRoot <0ventox0@gmail.com>
2021-06-29 11:40:57 +0200

    Added basic db functionality

--- WpfExplorer/MainWindow.xaml.cs
+++ WpfExplorer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static WpfExplorer.db;
 
 namespace WpfExplorer
 {
@@ -23,6 +24,11 @@ namespace WpfExplorer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            db.connect();
         }
     }
 }

9d7ef271675b4b004bf1cec6c87c2dcd05ccf9ab
VenRoot <0ventox0@gmail.com>
2021-06-30 15:49:37 +0200

    fs.cs erstellt #5

---
+++ WpfExplorer/fs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WpfExplorer
+{
+    class fs
+    {
+        public static string[] readDirSync(string path)
+        {
+            return Directory.GetFiles(path);
+        }
+
+        public static void writeFileSync(string path, string context, bool overwrite = false)
+        { 
+            if (!overwrite && File.Exists(path)) return;
+            File.WriteAllText(path, context);
+        }
+
+        public static bool exists(string path)
+        {
+            return File.Exists(path) || Directory.Exists(path);
+        }
+    }
+}

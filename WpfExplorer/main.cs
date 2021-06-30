768dc9cc1f7538fadbe1ed0d7a84bd94ba13fc0c
VenRoot <0ventox0@gmail.com>
2021-06-30 11:58:37 +0200

    Added main.cs and PingDB Method. Closes #1

---
+++ WpfExplorer/main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfExplorer
+{
+    public class main
+    {
+        public static void ReportError(Exception e)
+        {
+            if(e.GetType().ToString() == "Npgsql.NpgsqlException")
+            {
+                string msg = "Es wurde ein Fehler festgestellt. Stellen Sie sicher, dass sie mit dem Internet verbunden sind. Bei weiteren Fragen wenden Sie sich an ihren System Administrator\n\n\nFehlertext: " + e.Message;
+                MessageBox.Show(msg);
+                System.Environment.Exit(1);
+            }
+        }
+
+        public static bool PingDB()
+        {
+            List<string> cmd = db.query("SELECT 1+1;");
+            string msgS = "";
+            for(int i = 0; i < cmd.ToArray().Length; i++)
+            {
+                msgS += cmd[i];
+                msgS +="\n\n";
+            }
+            MessageBox.Show(msgS);
+            if (cmd[0] == "2") return true;
+            return false;
+        }
+    }
+}

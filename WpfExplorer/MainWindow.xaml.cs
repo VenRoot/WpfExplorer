ac79fc0b5c2ac2d71df69cb2d7256051e379c73e
VenRoot <0ventox0@gmail.com>
2021-06-30 11:51:29 +0200

    Closes #9

--- WpfExplorer/MainWindow.xaml.cs
+++ WpfExplorer/MainWindow.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using static WpfExplorer.db;
+using static WpfExplorer.main;
 
 namespace WpfExplorer
 {
@@ -24,11 +26,29 @@ namespace WpfExplorer
         public MainWindow()
         {
             InitializeComponent();
+            if(main.PingDB()) { TB_Ping.Text = "Connected"; }
+            else { TB_Ping.Text = "Connection failed..."; ReportError(new Exception("Ping not successfull")); return; }
+            DispatcherTimer dT = new System.Windows.Threading.DispatcherTimer();
+            dT.Tick += new EventHandler(SetPing);
+            dT.Interval = new TimeSpan(0, 0, 1);
+            dT.Start();
         }
 
+        private void SetPing(object sender, EventArgs e)
+        {
+            double PingTime = db.PingDB();
+            TB_PingTime.Text = $"{PingTime}ms";
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            db.connect();
+            List<string> x = db.query("SELECT 1+1;");
+            string res = "";
+            for (int i = 0; i < x.Count; i++)
+            {
+                res += x[i];
+                res += "\n\n";
+            }
+            MessageBox.Show(res);
         }
     }
 }

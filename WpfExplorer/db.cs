a7ec2de35e295e0285a97105b53577cc6537c13f
VenRoot <0ventox0@gmail.com>
2021-06-30 11:35:04 +0200

    Method db.PingDB now working. DB Connection #1

--- WpfExplorer/db.cs
+++ WpfExplorer/db.cs
@@ -2,22 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Npgsql;
-using NpgsqlTypes;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Windows.Threading;
+
+using static WpfExplorer.main;
 using System.Windows;
+using System.Net.NetworkInformation;
+using static WpfExplorer.MainWindow;
 
 namespace WpfExplorer
 {
     class db
     {
-        
-
         /**Method */
         public static Item getConf()
         {
@@ -30,19 +28,42 @@ namespace WpfExplorer
                 return items[0];
             }
         }
-
-        async public static void connect()
+        
+        public static double PingDB()
+        {
+            Item conf = getConf();
+            Ping p = new Ping();
+            PingReply r = p.Send(conf.Host);
+            if(r.Status == IPStatus.Success) return r.RoundtripTime;
+            return -1;
+            
+        }
+        
+        public static List<string> query(string command)
         {
             Item item = getConf();
             string conStr = $"Host={item.Host};Username={item.Username};Password={item.Password};Database={item.Database}";
-            MessageBox.Show(conStr);
+            //MessageBox.Show(conStr);
             var con = new NpgsqlConnection(conStr);
-            await con.OpenAsync();
-            NpgsqlCommand cmd = new NpgsqlCommand("SHOW DATABASES;", con);
-            var reader = await cmd.ExecuteReaderAsync();
-            while (await reader.ReadAsync()) { } 
-            MessageBox.Show(reader.GetString(0));
+            try { con.Open(); }
+            catch(Exception e) { ReportError(e); throw; }
+            NpgsqlCommand cmd = new NpgsqlCommand(command, con);
+            //cmd.Prepare();
+            var reader = cmd.ExecuteReader();
+            //string[] x = new string[10000];
+            List<string> result = new List<string>(); 
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++) result.Add(reader.GetValue(i).ToString());
+            };
+            reader.Close();
+            return result;
+            //Array.ForEach(x, Console.WriteLine);
+
+
+            //MessageBox.Show(reader.GetString(0));
         }
+
 
         public class Item
         {

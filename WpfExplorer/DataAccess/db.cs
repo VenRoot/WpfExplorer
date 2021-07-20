4733be51e843b1b1e042d87381cbe4c6357a4219
VenRoot <0ventox0@gmail.com>
2021-07-20 08:58:42 +0200

    MySql added

--- WpfExplorer/DataAccess/db.cs
+++ WpfExplorer/DataAccess/db.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using Npgsql;
 using Newtonsoft.Json;
 using System.IO;
+using MySql.Data;
 
 using System.Net.NetworkInformation;
 using System.Windows;
 using System.Text.Json;
 using Newtonsoft.Json.Linq;
+
 
 namespace WpfExplorer
 {

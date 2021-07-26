9dd03678e283e53cd646b293ec9c0a1d949e06c3
VenRoot <0ventox0@gmail.com>
2021-07-26 12:12:45 +0200

    #2 Changed to MVVM

--- WpfExplorer/ViewModel/MainWindowViewModel.cs
+++ WpfExplorer/ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using CommandHelper;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -72,16 +73,52 @@ namespace WpfExplorer.ViewModel
             tb_AddExceptionsText = "";
         }
 
-        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        public void tb_Search_TextChanged()
         {
-            if (!Equals(field, newValue))
+            FoundFiles.Clear();
+
+            /** Nichts eingegeben = nichts anzeigen */
+            if (tb_Search_Text.Length == 0) return;
+            /**Es sollten zuerst die Dateinamen und DANN erst Dateien mit dem Inhalt durchsucht werden */
+
+            var File = fs.searchFile(tb_Search_Text, false);
+            if (File.Count != 0)
             {
-                field = newValue;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-                return true;
+                string res = "";
+                foreach (var v in File)
+                {
+                    res += v.Filename + "\n";
+                    res += v.Path + "\n\n";
+                }
+
+                FoundFiles.Add(res+"\n");
             }
 
-            return false;
+
+            //fs.C_IZ _ = db.getConf<fs.C_IZ>("database");
+            //for (int i = 0; i < _.Paths.Length; i++)
+            //{
+
+            //    System.Windows.Controls.TextBox txt = new System.Windows.Controls.TextBox();
+            //    List<main.FileStructure> oof = fs.searchFile(tb_Search.Text, false);
+            //    oof.ForEach((p) =>
+            //    {
+            //        AddToGrid(p.Filename, p.Path);
+            //        txt.Text += $"\n\n{p.Filename} in {p.Path}";
+            //    });
+
+            //}
+
+
+        }
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, newValue)) return false;
+            
+            field = newValue;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
         }
 
         public List<string> getFileExceptions()
@@ -89,11 +126,32 @@ namespace WpfExplorer.ViewModel
             return new List<string>(FileExceptionList);
         }
 
+        private RelayCommand tb_Search_Command1;
+
+        //public ICommand tb_Search_Command
+        //{
+        //    get
+        //    {
+        //        if (tb_Search_Command1 == null)
+        //        {
+        //            tb_Search_Command1 = new RelayCommand(tb_Search_);
+        //        }
+
+        //        return tb_Search_Command1;
+        //    }
+        //}
+
+        private void tb_Search_(object commandParameter)
+        {
+        }
+
         public ICommand ButtonCommand { get; set; }
+        public ICommand tb_Search_Command { get; set; }
 
         public MainWindowViewModel()
         {
             ButtonCommand = new RelayCommand(o => Debug_Click());
+            tb_Search_Command = new RelayCommand(o => tb_Search_TextChanged());
         }
 
         public void Debug_Click()
@@ -103,10 +161,30 @@ namespace WpfExplorer.ViewModel
 
         public string tb_AddExceptionsText { get; set; } = null;
 
+        public string _tb_Search_Text;
+        public string tb_Search_Text {
+            get { return _tb_Search_Text; }
+            set
+            {
+                if(_tb_Search_Text != value)
+                {
+                    _tb_Search_Text = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs(tb_Search_Text));
+                    PropertyChanged(this, new PropertyChangedEventArgs(_tb_Search_Text));
+                    tb_Search_TextChanged();
+                }
+                
+            }
+        }
+
+
         public ObservableCollection<string> FileExceptionList { get; set; } = new ObservableCollection<string>();
+        public ObservableCollection<string> FoundFiles { get; set; } = new ObservableCollection<string>();
         //public ObservableCollection<string> MyProperty { get; set; } = new ObservableCollection<string>();
         private object selectedFileException;
 
         public object SelectedFileException { get => selectedFileException; set => SetProperty(ref selectedFileException, value); }
+
+        
     }
 }

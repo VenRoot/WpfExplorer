8c51b2e04735ed7b12ec4ac63beb7ee9133cb315
Bomber <dilvir@live.de>
2021-10-01 21:45:04 +0200

    #26  Rausschieben von den Models in eigenes Verzeichnis. Kleines Codeclean.

---
+++ WpfExplorer/Model/ScannedFiles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfExplorer.Model
+{
+        class ScannedFilesList
+        {
+            public List<ScannedFile> FilesOk;
+            public List<ScannedFile> FilesErr;
+        }
+
+        public class ScannedFile
+        {
+            public string FileName;
+            public string Path;
+        }
+}

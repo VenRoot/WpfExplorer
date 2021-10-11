ec62493daa27490763a6220dcf09a44c9d75f4bb
BomberUHD <38734407+BomberUHD@users.noreply.github.com>
2021-10-11 13:51:36 +0200

    Revert "#36 View erneuert"

--- WpfExplorer/View/MainWindow.xaml.cs
+++ WpfExplorer/View/MainWindow.xaml.cs
@@ -36,15 +36,6 @@ namespace WpfExplorer
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            // JC Load Image to View
-            //string img_path = System.IO.Directory.GetCurrentDirectory();
-            //img_path = img_path.Replace(@"WpfExplorer\bin\Debug", @"img\play_new.png");
-            //play_btn.Source = new BitmapImage(new Uri(img_path));
-            Detect_Click(sender, e);
-        }
-
-        private void lb_Exceptions_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
 
         }
     }

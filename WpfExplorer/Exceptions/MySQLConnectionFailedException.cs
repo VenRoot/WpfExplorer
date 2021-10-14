a368c3263580703c35db63039f9cd9b7358645cc
Ven <0ventox0@gmail.com>
2021-10-14 23:26:15 +0200

    #30 Exception hinzugefügt

---
+++ WpfExplorer/Exceptions/MySQLConnectionFailedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfExplorer.Exceptions
+{
+    public class MySQLConnectionFailedException : Exception
+    {
+        public MySQLConnectionFailedException(string message): base(message)
+        {
+            if(message == null) message = "Die Verbindung zum MySQL-Server konnte nicht hergestellt werden. Bitte prüfen Sie die Einstellungen";
+        }
+    }
+}
